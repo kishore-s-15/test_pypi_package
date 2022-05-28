f767c3c5d4139d9ce79c29a6f6aabcb907aff5a5
kishore-s-15 <skishore2602.dev@gmail.com>
2022-05-28 15:43:39 +0530

    Fixed bugs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email="<skishore2602.dev@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'testing', 'pypi'],
